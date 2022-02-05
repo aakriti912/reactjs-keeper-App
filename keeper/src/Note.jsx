25261d9d6be751a37b5629ecde790eb65e3b3480
aakriti <59227365+aakriti912@users.noreply.github.com>
2022-02-05 23:07:24 +0545

    Create Note.jsx

---
+++ keeper/src/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="container">
+      <h1>title of the project</h1>
+      <p>hssugshgdha hjasghsab</p>
+    </div>
+  );
+}
+export default Note;
