63f59ebb435ad5a23821de77e25f2f9624ca5613
aakriti <59227365+aakriti912@users.noreply.github.com>
2022-02-05 23:06:03 +0545

    Create Footer.jsx

---
+++ keeper/src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// const date = new Date();
+
+function Footer() {
+  return (
+    <footer>
+      <p>CopyRight 2019 </p>
+    </footer>
+  );
+}
+export default Footer;
